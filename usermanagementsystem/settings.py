3bbc78f1d729149ba2930597cc9b091ca157bd3a
Ankush31079 <122419934+Ankush31079@users.noreply.github.com>
2023-01-11 10:42:03 +0530

    Update settings.py

--- usermanagementsystem/settings.py
+++ usermanagementsystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&-f28_3oi@03cb&h5!+bd%zy%)r*3fc#s#6-4fawttgovot5wc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
        'NAME': 'userproject',
        'USER': 'ankush',
-       'PASSWORD': 'Welcome123@',
+       'PASSWORD': os.environ.get('PASSWORD')
        'HOST': 'localhost',
        'PORT': '3306'
         
